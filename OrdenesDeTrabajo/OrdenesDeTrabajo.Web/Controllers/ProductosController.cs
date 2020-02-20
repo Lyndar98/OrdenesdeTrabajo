ce64fe6729d967fa6add393482f661c5262cf238
germanm27 <gfmendoza@unah.hn>
2020-02-20 16:47:09 -0600

    Agregando Entity Framework Code First

---
+++ OrdenesDeTrabajo/OrdenesDeTrabajo.Web/Controllers/ProductosController.cs
@@ -0,0 +1,21 @@
+﻿using OrdenesDeTrabajo.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrdenesDeTrabajo.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            var productosBL = new ProductosBL();
+            var ListadeProductos = productosBL.ObtenerProductos();
+
+            return View(ListadeProductos);
+        }
+    }
+}
